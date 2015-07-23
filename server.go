c93e94e3f39e278ca6fb821e2f6b732260887a40
katerinasoza <azzakrosh@xyzrd.com>
2015-07-23 13:26:10 +0300

    Update server.go

--- server.go
+++ server.go
@@ -9,20 +9,14 @@ import (
 
 var msgs []string
 
-type Page struct { 
-    Body  string
-}
-
 func timegetter(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintln(w, "Here's your time:\n", time.Now().UTC())
 }
 
-func poster (w http.ResponseWriter, r *http.Request) {
-	var p Page
+func poster (w http.ResponseWriter, r *http.Request) { 
 	t, _ := template.ParseFiles("post.html")
-	t.Execute(w, p)
-	msgs = append(msgs, p.Body)
-	fmt.Fprintln(w, msgs)
+	t.Execute(w, msgs)
+	msgs = append(msgs, r.FormValue("body"))
 }
 
 func allmessages (w http.ResponseWriter, r *http.Request) {
