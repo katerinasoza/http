88e719c05aeda6e7f9b8f2cd5cb9436ec4788d9f
katerinasoza <azzakrosh@xyzrd.com>
2015-07-29 14:09:15 +0300

    Update server.go

--- server.go
+++ server.go
@@ -9,22 +9,17 @@ import (
 )	
 
 var  (msgs []string
-	 rw *sync.RWMutex)
+	 rw sync.RWMutex)
 
 func timegetter(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintln(w, "Here's your time:\n", time.Now().UTC())
 }
 
-func poster (w http.ResponseWriter, r *http.Request) {
+func parser (w http.ResponseWriter, r *http.Request){
 	t, err1 := template.ParseFiles("post.html")
 	if err1 != nil {
 		http.Error(w, err1.Error(), http.StatusInternalServerError)
 		return
-	}
-	if r.FormValue("body") != ""{
-		rw.Lock()
-		msgs = append(msgs, r.FormValue("body"))
-		rw.Unlock()
 	}
 	err := t.Execute(w, msgs)
 	if err != nil {
@@ -32,7 +27,16 @@ func poster (w http.ResponseWriter, r *http.Request) {
 	}
 }
 
+func poster (w http.ResponseWriter, r *http.Request) {
+	if r.FormValue("body") != ""{
+		rw.Lock()
+		msgs = append(msgs, r.FormValue("body"))
+		rw.Unlock()
+	}
+}
+
 func main(){
+	parser()
 	http.HandleFunc("/time", timegetter)
 	http.HandleFunc("/message", poster)
 	http.ListenAndServe(":8080", nil)
