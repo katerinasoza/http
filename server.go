f02905957b40ae307eb616b739c3bb42968a0044
katerinasoza <azzakrosh@xyzrd.com>
2015-07-30 11:17:27 +0300

    Update server.go

--- server.go
+++ server.go
@@ -6,38 +6,57 @@ import (
 	"fmt"
 	"time"
 	"sync"
+	"log"
 )	
 
-var  (msgs []string
-	 rw sync.RWMutex)
+type Message struct{
+	msgs []string
+	mu   sync.Mutex
+	tmpl *template.Template
+}  
 
-func timegetter(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintln(w, "Here's your time:\n", time.Now().UTC())
+func (m *Message) NewMessage (name string) *Message{ //инициализатор
+	m.msgs = make([]string, 0, 100)
+	var err error
+	if m.tmpl, err = template.ParseFiles(name); err != nil {
+		log.Fatal(err.Error())
+	}
+	return m
 }
 
-func parser (w http.ResponseWriter, r *http.Request){
-	t, err1 := template.ParseFiles("post.html")
-	if err1 != nil {
-		http.Error(w, err1.Error(), http.StatusInternalServerError)
-		return
+func (m *Message) Add (msg string){
+	if  msg != ""{
+		m.mu.Lock()
+		m.msgs = append(m.msgs, msg)
+		m.mu.Unlock()
 	}
-	err := t.Execute(w, msgs)
-	if err != nil {
+}
+func (m *Message) Clear () {
+	m.msgs = make([]string, 0, 100)
+	return 
+}
+
+func (m *Message) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	if err := m.tmpl.Execute(w, m.msgs); err != nil {
 		http.Error(w, err.Error(), http.StatusInternalServerError)
 	}
-}
-
-func poster (w http.ResponseWriter, r *http.Request) {
-	if r.FormValue("body") != ""{
-		rw.Lock()
-		msgs = append(msgs, r.FormValue("body"))
-		rw.Unlock()
-	}
-}
+	if r.Method == "POST" {
+		if r.FormValue("name") != "" {
+		m.Clear()
+		}
+		msg := r.FormValue("body")
+		if msg != "" {
+			m.Add(msg)
+		}
+}}
 
 func main(){
-	parser()
-	http.HandleFunc("/time", timegetter)
-	http.HandleFunc("/message", poster)
-	http.ListenAndServe(":8080", nil)
+	var m *Message 
+	m.NewMessage("post.html") 
+	log.Println(m)
+	http.HandleFunc("/time", func (w http.ResponseWriter, r *http.Request) {fmt.Fprintln(w, time.Now().UTC() .Format("2006-01-02T15:04:05Z07:00"))})
+	http.HandleFunc("/message", m.ServeHTTP)
+	if err :=  http.ListenAndServe(":8080", nil); err != nil{
+		log.Fatal(err.Error())
+	}
 }
